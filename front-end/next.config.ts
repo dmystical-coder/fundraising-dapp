f90abbde1a2ae22b5af7eb8fe051adcb88416318
dmystical-coder <dmystical.coder@gmail.com>
2025-12-24 10:40:41 +0100

    fix(wallet): add transpilePackages for @stacks/connect
    - Added transpilePackages config for @stacks/connect and @stacks/connect-ui
    - Fixes ChunkLoadError when loading connect modal

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // Transpile @stacks packages to fix dynamic import issues with connect modal
+  transpilePackages: [
+    "@stacks/connect",
+    "@stacks/connect-ui",
+  ],
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
@@ -8,3 +13,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
